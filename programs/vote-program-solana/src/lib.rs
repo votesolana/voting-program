27eb556778af7e3d94e075bce7713543eba20a16
taborgreat <taborgreat@gmail.com>
2024-06-26 14:38:31 -0700

    wokring on voiting program and tests

--- programs/vote-program-solana/src/lib.rs
+++ programs/vote-program-solana/src/lib.rs
@@ -1,37 +1,70 @@
 use anchor_lang::prelude::*;
-
 use anchor_spl::{
-    token::{Mint, Token, TokenAccount}
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount, Transfer, transfer}
 };
 
-declare_id!("6GBHN1ddUDKtf7kaJ5BwoHzVPxXDMKaATS4WuXe4TFAg");
+use solana_program::clock::Clock;
+
+declare_id!("96Gwwh7req3ypdcr9uHePeeYonqqJisRrjn8C9Nywgb7");
 
 pub mod constants {
-    pub const TREASURY_SEED: &[u8] = b"vote_vault";
-    pub const VOTE_INFO_SEED: &[u8] = b"voting_info";
-    pub const TOKEN_WALLET_SEED: &[u8] = b"token";
+    pub const TREASURY_SEED: &[u8] = b"vote_vaulttremp";
+    pub const VOTE_INFO_SEED: &[u8] = b"votewiftremp_info";
+    pub const TOKEN_WALLET_SEED: &[u8] = b"votewiftremptoken";
 }
 
 #[program]
 pub mod vote_program_solana {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 
-    pub fn vote(ctx: Context<Initialize>, amount: u64) -> Result<()> { //interval (macro of time staked 1day, 7day, erc), wifTremp (boolean)
+    pub fn vote(ctx: Context<Vote>, amount: u64) -> Result<()> {
+        let vote_info_account = &mut ctx.accounts.vote_info_account;
+
+        if vote_info_account.has_voted {
+            return Err(ErrorCode::IsVoted.into());
+        }
+
+        if amount <= 100 {
+            return Err(ErrorCode::TooLow.into());
+        }
+
+        let clock = Clock::get()?;
+
+        vote_info_account.voted_at_slot = clock.slot;
+        vote_info_account.has_voted = true;
+
+        let vote_amount = (amount)
+            .checked_mul(10u64.pow(ctx.accounts.mint.decimals as u32))
+            .unwrap();
+                //1.000000
+        transfer(
+            CpiContext::new(
+              ctx.accounts.token_program.to_account_info(),
+              Transfer {
+                from:ctx.accounts.user_votewiftremp_account.to_account_info(),
+                to: ctx.accounts.vote_account.to_account_info(),
+                authority: ctx.accounts.signer.to_account_info(),
+              }
+            ),
+            vote_amount, //+reward pool amount and transfer from reward pool to vote_account
+        )?;
+        
+
         Ok(())
     }
 
-    pub fn collect_vote(ctx: Context<Initialize>) -> Result<()> {
+    pub fn collect_vote(ctx: Context<CollectVote>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-
     #[account(mut)]
     pub signer: Signer<'info>,
 
@@ -44,8 +77,80 @@ pub struct Initialize<'info> {
         token::authority = treasury_account,
     )]
     pub treasury_account: Account<'info, TokenAccount>,
+
     pub mint: Account<'info, Mint>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Vote<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::VOTE_INFO_SEED, signer.key().as_ref()],
+        bump,
+        payer = signer,
+        space = 8 + std::mem::size_of::<VoteInfo>(),
+    )]
+    pub vote_info_account: Account<'info, VoteInfo>,
+
+    #[account(
+        init_if_needed,
+        seeds = [constants::TOKEN_WALLET_SEED, signer.key().as_ref()],
+        bump,
+        payer = signer,
+        token::mint = mint,
+        token::authority = vote_account
+    )]
+    pub vote_account: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+    )]
+    pub user_votewiftremp_account: Account<'info, TokenAccount>,
+
+    pub mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CollectVote<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [constants::TREASURY_SEED],
+        bump
+    )]
+    pub treasury_account: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct VoteInfo {
+    pub voted_at_slot: u64, // Exact time slot vote is placed
+    pub has_voted: bool,
+    pub wif_tremp: bool, // Voted for Tremp or Boden
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You have already voted")]
+    IsVoted,
+    #[msg("You don't have a vote to claim")]
+    NotVoted,
+    #[msg("You must wait longer to claim")]
+    TimeLocked,
+    #[msg("You need at least 100 tokens to vote")]
+    TooLow,
+}
--- tests/vote-program-solana.ts
+++ tests/vote-program-solana.ts
@@ -1,8 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { VoteProgramSolana } from "../target/types/vote_program_solana";
-import { Connection, Keypair } from "@solana/web3.js";
-import { createMint } from "@solana/spl-token";
+import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { createMint, getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
 
 describe("vote-program-solana", () => {
   // Configure the client to use the local cluster.
@@ -10,9 +10,15 @@ describe("vote-program-solana", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
   const payer = provider.wallet as anchor.Wallet;
   const connection = new Connection("http://127.0.0.1:8899", "confirmed");
-  const mintKeypair = Keypair.generate();
+  const mintKeypair = Keypair.fromSecretKey(new Uint8Array([
+    106, 161, 133, 170,  47,  84,  34,  10,  51,  38, 126,
+     28, 165,  73, 222,  35,  53, 223, 147, 161,  27,  93,
+    127,  21,  79, 214, 100, 117,  39, 240,   7, 203, 122,
+    197,  84, 167, 209, 141, 227, 190,  41, 174, 122, 255,
+     21, 102,  98,  37, 100,  19, 144,  78, 251, 179, 167,
+    150, 118, 127,   0, 232, 131,  29,   3, 244
+  ]));
 
-  console.log(mintKeypair)
 
   const program = anchor.workspace.VoteProgramSolana as Program<VoteProgramSolana>;
 
@@ -30,10 +36,73 @@ describe("vote-program-solana", () => {
   }
 
   it("Is initialized!", async () => {
-    await createMintToken();
-    const tx = await program.methods.initialize().rpc();
+    //await createMintToken();
+
+    let [treasuryAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("vote_vaulttremp")],
+      program.programId
+    )
+
+    console.log(treasuryAccount)
+    const tx = await program.methods.initialize()
+    .accounts({
+      signer: payer.publicKey,
+      treasury_account: treasuryAccount,
+      mint: mintKeypair.publicKey
+    } )
+    .rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("vote", async() => {
+    let userVotewiftrempAccount = await  getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeypair.publicKey,
+      payer.publicKey
+    );
+
+    await mintTo(
+      connection,
+      payer.payer,
+      mintKeypair.publicKey,
+      userVotewiftrempAccount.address,
+      payer.payer,
+      1e7
+    )
+
+    let [voteInfo] = PublicKey.findProgramAddressSync(
+      [Buffer.from("votewiftremp_info"), payer.publicKey.toBuffer()],
+      program.programId
+    )
+
+    let [voteAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("votewiftremptoken"), payer.publicKey.toBuffer()],
+      program.programId
+    )
+
+    await getOrCreateAssociatedTokenAccount(
+      connection,
+      payer.payer,
+      mintKeypair.publicKey,
+      payer.publicKey
+    )
+
+
+    const tx = await program.methods
+      .vote(new anchor.BN(101))
+      .signers([payer.payer])
+      .accounts({
+        voteInfoAcount: voteInfo,
+        voteAccount: voteAccount,
+        userVotewiftrempAccount: userVotewiftrempAccount.address,
+        mint: mintKeypair.publicKey,
+        signer: payer.publicKey
+      })
+      .rpc();
+
+      console.log("Your transaction signature", tx);
+  })
 });
 
 
